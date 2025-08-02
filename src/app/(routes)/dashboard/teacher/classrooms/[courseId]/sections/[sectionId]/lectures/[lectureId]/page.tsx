b2ac9cfe43473ffe50159bf405d37df9c8fd8c6a
Osank31 <osankverma2004@gmail.com>
2025-08-02 10:44:18 +0530

    feat: implement lecture data fetching in Page component and add GET route for lecture retrieval

--- src/app/(routes)/dashboard/teacher/classrooms/[courseId]/sections/[sectionId]/lectures/[lectureId]/page.tsx
+++ src/app/(routes)/dashboard/teacher/classrooms/[courseId]/sections/[sectionId]/lectures/[lectureId]/page.tsx
@@ -1,13 +1,38 @@
 'use client';
 
+import Loading from "@/app/_components/Loading";
+import { Lecture } from "@/types/classroomsCreated";
+import axios from "axios";
 import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
-function page() {
-    const {courseId, sectionId, lectureId} = useParams();
-  return (
-    <div>page
-        {courseId} - {sectionId} - {lectureId}
-    </div>
-  )
+export default function Page() {
+    const {lectureId} = useParams();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    const [lectureData, setLectureData] = useState<Lecture | null>(null);
+
+    useEffect(()=>{
+        const fetchLectureData = async () => {
+            try {
+                const response = await axios.get(`/api/lecture?lectureId=${lectureId}`);
+                setLectureData(response.data.data);
+            } catch (error) {
+                setError("Failed to fetch lecture data");
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchLectureData();
+    },[lectureId])
+    console.log(lectureData)
+    if (loading) {
+        return <Loading />;
+    }
+    if (error) {
+        return <div className="text-red-500">{error}</div>;
+    }
+    return (
+        <div>page</div>
+    )
 }
-export default page
--- src/app/api/(teacher)/lecture/route.ts
+++ src/app/api/(teacher)/lecture/route.ts
@@ -4,6 +4,7 @@ import Response from "@/helpers/Response";
 import { db } from "@/config/db";
 import { lectureTable } from "@/config/schema";
 import { lectureSchema } from "@/helpers/validators/lectureSchema";
+import { eq } from "drizzle-orm";
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
@@ -29,6 +30,27 @@ export async function POST(request: NextRequest) {
         }
         await db.insert(lectureTable).values(safeResult.data).returning();
         return Response.success(safeResult.data, "Lecture created successfully", 201);
+    } catch (error) {
+        return Response.error(error, "Internal Server Error", 500);
+    }
+}
+
+export async function GET(request: NextRequest) {
+    const user = (await currentUser()); // Mock user for testing, remove in production
+    if (!user) {
+        return Response.error(null, "Unauthorized", 401);
+    }
+    const {searchParams} = new URL(request.url)
+
+    const lectureId = searchParams.get('lectureId');
+    if (!lectureId) {
+        return Response.error(null, "Lecture ID is required", 400);
+    }
+    
+    try {
+        const result = await db.select().from(lectureTable).where(eq(lectureTable.id, lectureId))
+
+        return Response.success(result[0], "Lecture retrieved successfully", 200);
     } catch (error) {
         return Response.error(error, "Internal Server Error", 500);
     }
